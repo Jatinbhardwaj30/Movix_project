15b1a67c77710a29494520ffb8831f9db9c36d27
Jatinbhardwaj30 <bhardwajjatin727@gmail.com>
2025-06-27 16:16:53 +0530

    changes

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -3,9 +3,9 @@
 
 export const getNowPlayingMovies = async (req, res) => {
     try {
-        const movies = await Movie.find({}).sort({ release_date: -1 }).limit(10);
-        res.status(200).json(movies);
+        // const movies = await Movie.find({}).sort({ release_date: -1 }).limit(10);
+        // res.status(200).json(movies);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        // res.status(500).json({ message: error.message });
     }
 }
